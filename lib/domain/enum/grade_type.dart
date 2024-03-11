b7a26c1a7eff0971e7f814a55bab0242a307c193
HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>
2024-03-11 20:05:01 +0900

    feat: Grade 에셋 추가
    Signed-off-by: HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>

--- lib/domain/enum/grade_type.dart
+++ lib/domain/enum/grade_type.dart
@@ -14,7 +14,25 @@ enum GradeType {
   Ap("a_p.png"),
   A("a.png"),
   B("b.png"),
-  C("c.png");
+  C("c.png"),
+  D("d.png"),
+  F("f.png"),
+  xSSSp("x_sss_p.png"),
+  xSSS("x_sss.png"),
+  xSSp("x_ss_p.png"),
+  xSS("x_ss.png"),
+  xSp("x_s_p.png"),
+  xS("x_s.png"),
+  xAAAp("x_aaa_p.png"),
+  xAAA("x_aaa.png"),
+  xAAp("x_aa_p.png"),
+  xAA("x_aa.png"),
+  xAp("x_a_p.png"),
+  xA("x_a.png"),
+  xB("x_b.png"),
+  xC("x_c.png"),
+  xD("x_d.png"),
+  xF("x_f.png");
 
   final String fileName;
   const GradeType(this.fileName);
