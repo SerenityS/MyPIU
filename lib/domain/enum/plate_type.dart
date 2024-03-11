92f8c157d7e782f5bf00535be18286f0963558c2
HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>
2024-03-11 19:57:32 +0900

    feat: Plate 어셋 추가
    Signed-off-by: HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>

--- lib/domain/enum/plate_type.dart
+++ lib/domain/enum/plate_type.dart
@@ -1,17 +1,18 @@
 // ignore_for_file: constant_identifier_names
 
 enum PlateType {
-  PG("pg.png"),
-  UG("ug.png"),
-  EG("eg.png"),
-  SG("sg.png"),
-  MG("mg.png"),
-  TG("tg.png"),
-  FG("fg.png"),
-  RG("rg.png");
+  PG("pg.png", "s_pg.png"),
+  UG("ug.png", "s_ug.png"),
+  EG("eg.png", "s_eg.png"),
+  SG("sg.png", "s_sg.png"),
+  MG("mg.png", "s_mg.png"),
+  TG("tg.png", "s_tg.png"),
+  FG("fg.png", "s_fg.png"),
+  RG("rg.png", "s_rg.png");
 
   final String fileName;
-  const PlateType(this.fileName);
+  final String smallFileName;
+  const PlateType(this.fileName, this.smallFileName);
 
   static PlateType fromString(String text) {
     for (PlateType entry in PlateType.values) {
